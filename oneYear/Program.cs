f2ded01c13d5dfddebdcfc8e840b903fff3a4608
Kamran Z <kamraneze@code.edu.az>
2023-08-22 11:02:39 +0400

    All Month in One Year

---
+++ oneYear/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace oneYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            var result = (OneYear)number;
+            Console.WriteLine(result);
+        }
+    }
+}
